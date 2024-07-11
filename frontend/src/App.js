514b683a4490041a8d1fb553576a55ed5a9a2b52
Eduardo <esb00016@red.ujaen.es>
2024-07-11 22:27:36 +0200

    Prepare for Heroku deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         formData.append('file', file);
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/upload', {
+            const response = await fetch('https://valvulopathy-health.herokuapp.com/upload', {
                 method: 'POST',
                 body: formData,
             });
